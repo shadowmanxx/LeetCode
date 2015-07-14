3e4783bfdffeb653f732ddc8350e1d0c8fb00941
shadowmanxx <shadowmanxx@163.com>
2015-07-14 17:02:20 +0800

    Create code.c

---
+++ Easy/Isomorphic-Strings/code.c
@@ -0,0 +1,19 @@
+bool isIsomorphic(char* s, char* t) {
+    char charArrS[256] = { 0 };
+    char charArrT[256] = { 0 };
+    int i = 0;
+    while (s[i] !=0)
+    {
+        if (charArrS[s[i]] == 0 && charArrT[t[i]] == 0)
+        {
+            charArrS[s[i]] = t[i];
+            charArrT[t[i]] = s[i];
+        }
+        else
+        if (charArrS[s[i]] != t[i] || charArrT[t[i]] != s[i])
+            return false;
+        i++;
+    }
+
+    return true;
+}
